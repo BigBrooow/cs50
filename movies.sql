d565cb2e035dd3dd9d8189ba73da8bdf632fcfb5
BigBrooow <104306491+BigBrooow@users.noreply.github.com>
2022-07-15 19:16:28 +0300

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -42,6 +42,6 @@ WHERE people.name = "Johnny Depp" AND movies.title IN (SELECT title FROM movies 
 INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Helena Bonham Carter");
 
 --13
-SELECT movies.title FROM movies INNER JOIN stars ON movies.id = stars.movie_id INNER JOIN people ON stars.person_id = people.id
-WHERE people.name = "Johnny Depp" AND movies.title IN (SELECT title FROM movies INNER JOIN stars ON movies.id = stars.movie_id
-INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Helena Bonham Carter");
+SELECT DISTINCT people.name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
+WHERE movies.id IN (SELECT movies.id FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
